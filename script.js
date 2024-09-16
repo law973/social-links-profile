b98406f57a01cbe931dd8ff2ea4c40c07dfd07bb
Levi Wright <lawright379@gmail.com>
2024-09-16 13:29:06 -0300

    added modern css reset and button for theme switching

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 // Clicking the profile image changes the page color
 const themeSwitcher = document.getElementById("theme-switcher");
 
-function switchTheme() {
+function determineNewTheme() {
     const currentTheme = document.documentElement.getAttribute('data-theme');
     const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+
+    return newTheme;
+}
+
+function switchToNewButtonText(newTheme) {
+    const newButtonText = newTheme === 'light' ? 'Switch to Dark Mode' : 'Switch to Light Mode';
+    themeSwitcher.innerHTML = newButtonText;
+}
+
+function switchToNewTheme() {
+    const newTheme = determineNewTheme();
+    switchToNewButtonText(newTheme);
     document.documentElement.setAttribute('data-theme', newTheme);
 }
 
 // Event Listener
-themeSwitcher.addEventListener('click', switchTheme);
+themeSwitcher.addEventListener('click', switchToNewTheme);
